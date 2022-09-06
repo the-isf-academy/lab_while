c64e8a2b1e1671c9cbaf643b4e0f9e70793a0c83
emma <eqbrown@isf.edu.hk>
2022-09-06 11:04:36 +0800

    add pseudocode comments

--- hailstone_sequence.py
+++ hailstone_sequence.py
@@ -6,4 +6,6 @@
 print("--- Hailstone Sequence ---")
 print()
 
-# YOUR CODE GOES HERE
+# get the starting number from the user 
+# create a variable to count the number of steps it takes to find the sequence
+# loop until the number is equal to 1
