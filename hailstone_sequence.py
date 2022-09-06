f7a23716606b804da94e0373088dbc48e65f76d7
emma <eqbrown@isf.edu.hk>
2022-09-06 11:02:25 +0800

    hailstone solution

--- hailstone_sequence.py
+++ hailstone_sequence.py
@@ -6,4 +6,17 @@
 print("--- Hailstone Sequence ---")
 print()
 
-# YOUR CODE GOES HERE
+num = int(input("Input a starting number: "))
+steps_to_end = 0
+
+while num != 1:
+    if num%2 == 0:
+        num = num//2
+    else:
+        num = num*3+1
+
+    steps_to_end += 1
+
+    print(num)
+
+print("It took {} steps to complete the sequence".format(steps_to_end))
